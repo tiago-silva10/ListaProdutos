570f1969ddd8b824e2ed21897c1d490329e5c04e
tiago-silvaa10 <tiago_silvaa10@outlook.com>
2016-10-10 20:49:44 -0300

    criando projeto

---
+++ Avaliacao/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Avaliacao.DAL;
+using Avaliacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+
+namespace Avaliacao.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index(int? pagina)
+        {
+            IList<Product> lista = MockProductDao.GetAllProducts();
+            return View(lista.ToPagedList(pagina ?? 1, 10));
+        }
+    }
+}
