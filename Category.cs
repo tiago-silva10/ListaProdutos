a83c3a5b4c2cbf2de63b68632a624719b2828804
Tiago do Nascimento Silva <tiago_silvaa10@outlook.com>
2016-10-10 21:02:22 -0300

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avaliacao.Models
+{
+    public class Category
+    {
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Avaliacao.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            Id = 0;
+        }
+        public Product(int id, string name, decimal price, IEnumerable<Category> categories, IEnumerable<string> tags, bool isDisabled = false)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Categories = categories.ToList();
+            Tags = tags.ToList();
+            IsDisabled = isDisabled;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<string> Tags { get; set; }
+        public bool IsDisabled { get; set; }
+    }
+}
